a6decb335d1a37ceb14850ac1fe0d7bc23932e28
piupaulgit <contactpiu619@gmail.com>
2024-08-21 22:45:07 +0530

    useTimer hook

---
+++ react-prep/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import UseTimerComponent from "./components/userTimerComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <UseTimerComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-prep/src/components/userTimerComponent.js
@@ -0,0 +1,21 @@
+import React, { useEffect } from "react";
+import useTimer from "../hooks/useTimer";
+
+const UseTimerComponent = () => {
+  const { isRunning, start, stop, seconds } = useTimer(10);
+
+  return (
+    <div>
+      <h2>Timer Component</h2>
+      {isRunning ? <h3>{seconds}</h3> : <p>Timer not running</p>}
+      <button onClick={start} disabled={isRunning}>
+        Start
+      </button>
+      <button onClick={stop} disabled={!isRunning}>
+        Stop
+      </button>
+    </div>
+  );
+};
+
+export default UseTimerComponent;
