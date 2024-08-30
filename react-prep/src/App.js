6bc8a1dae0e1509f2a9736a6a82fc82d38f24ddf
piupaulgit <contactpiu619@gmail.com>
2024-08-30 22:21:14 +0530

    linked list issues

--- react-prep/src/App.js
+++ react-prep/src/App.js
@@ -2,18 +2,21 @@ import "./App.css";
 import UseTimerComponent from "./customHooks/useTimer/userTimerComponent";
 import Sidebar from "./TreeLinkSidebar/Sidebar";
 import { DATA } from "./country_capitalGame/Data";
+import CHART_DATA from "./velocity_chart/data";
 import Game from "./country_capitalGame/Game";
 import Card from "./card_select_deSelect_same_order/Card";
+import Chart from "./velocity_chart/Chart";
 
 function App() {
   return (
     <div className="App">
       <UseTimerComponent />
-      <hr />
+      {/* <hr />
       <Game data={DATA} />
       <hr />
       <Sidebar />
-      <hr />
+      <hr /> */}
+      <Chart data={CHART_DATA} />
     </div>
   );
 }
